d8811efde551c743c8b123a206b4c660fa32bdf0
limitimil <zlsh10340@gmail.com>
2020-04-05 02:32:03 +0800

    MAIN: api to util ci appender

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import requests
 from flask import jsonify
 from flask_cors import CORS
 from flask import Flask
+from flask import request
 
 from werkzeug.exceptions import HTTPException
 
 import os
 import logging
 import logging.config
+
+from jira_services.comments_appender import append_ci_reference
 
 _PATH = os.path.dirname(os.path.abspath(__file__))
 _PATH = os.path.join(_PATH, 'logging.ini')
@@ -20,6 +23,12 @@ logger2 = logging.getLogger('flask')
 app = Flask(__name__)
 app.config['CORS_EXPOSE_HEADERS'] = 'Content-Disposition'
 CORS(app)
+
+@app.route('/append_comment/ci_reference', methods=['PUT'])
+def ci_reference():
+    append_ci_reference(** request.json)
+    return 'OK', 200
+
 
 @app.errorhandler(Exception)
 def handle_error(e):
