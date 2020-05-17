a93d5a4a261dcc7cd2c08240952911f9f989afd8
limitimil <liminchien@cybersoft4u.com>
2020-05-17 17:59:24 +0800

    JIRA: append change log

--- jira_services/comments_appender.py
+++ jira_services/comments_appender.py
@@ -23,3 +23,22 @@ def append_ci_reference(issue_key, reference_url):
     issue = jira.issue(issue_key)
     jira.add_comment(issue, '* *CI:*\n** [{url}|{url}] '.format(url=reference_url))
 
+def append_change_log(issue_key, change_logs: list):
+    title = "Change log"
+    issue = jira.issue(issue_key)
+    last_comment = issue.fields.comment.comments[-1]
+    raw_markdown = last_comment.body
+    jmh = JiraMarkdownHelper(raw_markdown)
+    insert_point = jmh.get_insert_point(title)
+
+    raw_change_logs = ""
+    for log in change_logs:
+        raw_change_logs = raw_change_logs + '** ' + '{{{{{}}}}} '.format(log[0]) + ''.join(log[1:]) + '\n'
+    if insert_point is not None:
+        raw_markdown = jmh.insert_content(
+            raw_change_logs,
+            insert_point
+        )
+        last_comment.update(body=raw_markdown)
+    else:
+        jira.add_comment(issue, '* *{}:*\n'.format(title) + raw_change_logs)
