471e082960f93731c09e609bc8901ee48ba42e78
limitimil <liminchien@cybersoft4u.com>
2020-06-14 22:29:30 +0800

    HELPER: aggregate function

--- jira_services/jira_markdown_helper.py
+++ jira_services/jira_markdown_helper.py
@@ -15,3 +15,22 @@ class JiraMarkdownHelper:
     def insert_content(self, content, insert_line=-1):
         fragments = self.raw_markdown.split('\n')
         return '\n'.join(fragments[:insert_line] + [content] + fragments[insert_line:])
+    def aggregate_content(self, title, max_size=10):
+        pattern1 = '^\* .*{}.*'.format(title)
+        pattern2 = '^\*\* .*'
+        title_presented_index = -1
+        for index, line in enumerate(self.raw_markdown.split('\n')):
+            if title_presented_index != -1 and not re.match(pattern2, line):
+                index = index -1
+                break
+            if re.match(pattern1, line):
+                title_presented_index = index
+        if index - title_presented_index > max_size:
+            fragments = self.raw_markdown.split('\n')
+            return '\n'.join(
+                fragments[:title_presented_index]+
+                [fragments[title_presented_index] + '(10+)'] +
+                fragments[index+1:]
+                )
+        else:
+            return self.raw_markdown
--- unittest/test_jira_markdown_helper.py
+++ unittest/test_jira_markdown_helper.py
@@ -1,8 +1,7 @@
 # -*- encoding: utf-8 -*-
 import unittest
 from jira_services.jira_markdown_helper import JiraMarkdownHelper
-RAW_MARKDWON="""
-* *helloworld:*
+TEST_DATA_1="""* *helloworld:*
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
@@ -15,31 +14,33 @@ RAW_MARKDWON="""
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
-* *hellohell:*
+** """
+EXPECTED_RESULT_1="""* *helloworld:*(10+)"""
+TEST_DATA_2="""* *helloworld:*
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
 ** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+** [http://google.com|http://google.com] 
+* hellohell
 ** [http://google.com|http://google.com] 
 """
-AGGREGATED_MARKDWON="""
-* *helloworld:*(10+)
-* *hellohell:*
-** [http://google.com|http://google.com] 
-** [http://google.com|http://google.com] 
-** [http://google.com|http://google.com] 
-** [http://google.com|http://google.com] 
-** [http://google.com|http://google.com] 
-** [http://google.com|http://google.com] 
+EXPECTED_RESULT_2="""* *helloworld:*(10+)
+* hellohell
 ** [http://google.com|http://google.com] 
 """
 class TestJiraMarkdownHelper(unittest.TestCase):
     def test_aggregate_content(self):
-        jmh = JiraMarkdownHelper(RAW_MARKDWON)
+        jmh = JiraMarkdownHelper(TEST_DATA_1)
         result = jmh.aggregate_content('helloworld')
-        self.assertEqual(result , AGGREGATED_MARKDWON)
+        self.assertEqual(result , EXPECTED_RESULT_1)
 
         jmh = JiraMarkdownHelper(TEST_DATA_2)
         result = jmh.aggregate_content('helloworld')
