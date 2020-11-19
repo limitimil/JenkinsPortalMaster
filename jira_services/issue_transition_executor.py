a579e495ecedefea553f0e650fb069789b63bd53
limitimil <liminchien@cybersoft4u.com>
2020-11-19 23:57:48 +0800

    ITE: get by status name

--- jira_services/issue_transition_executor.py
+++ jira_services/issue_transition_executor.py
@@ -1,6 +1,12 @@
+from jira_services.core import jira
+
 from jira_services.core.issue_grabber import IssueGrabber
 
 class IssueTransitionExecutor(IssueGrabber):
     def transit_to_default_verifier(self):
         raise NotImplementedError
+
+    def get_transition_id_by_status_name(self, status_name='Verification'):
+        transitions = jira.transitions(self.issue)
+        return next(( t for t in transitions if t['to']['name'] == status_name), None)
 
