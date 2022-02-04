75d2a71108bb4d86d0d9b1fd0007c5992ee518a1
limitimil <zlsh10340@gmail.com>
2022-02-04 05:37:22 -0800

    Issue Grabber: get detail info

--- jira_services/core/issue_grabber.py
+++ jira_services/core/issue_grabber.py
@@ -5,3 +5,6 @@ class IssueGrabber:
         self.issue = jira.issue(issue_key)
         self.last_comment = self.issue.fields.comment.comments[-1]
         self.raw_markdown = self.last_comment.body
+        self.summary = self.issue.fields.summary
+        self.description = self.issue.fields.description
+        self.issue_key = issue_key
